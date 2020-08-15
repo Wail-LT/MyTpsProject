5213b41e0d23c453f958497bc62a6b4ec4d91583
Wells <latif.wail@hotmail.com>
2020-08-15 11:26:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyTpsProject
+My first game using Uneral engine 4
+Developed in C++ with Unreal Engine 4
---
+++ Source/MyTpsProject/MyTpsProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyTpsProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyTpsProject, "MyTpsProject" );
